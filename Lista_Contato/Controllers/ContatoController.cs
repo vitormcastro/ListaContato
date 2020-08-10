4df7041941c12dd05b76a53e67a98ffd1910938b
Vítor Marques de Castro <vitormcastro@outlook.com>
2020-08-10 01:13:36 -0300

    Add do controller do Contato

---
+++ Lista_Contato/Controllers/ContatoController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lista_Contato.Controllers
+{
+    public class ContatoController : Controller
+    {
+
+        private Models.ContatoEntities db = new Models.ContatoEntities();
+
+        // GET: Contato
+        public ActionResult Index()
+        {
+            List<Models.Contato> contatoList = null;
+            if (ModelState.IsValid)
+            {
+                contatoList = db.Contato.ToList();
+            }
+
+            return View(contatoList);
+       
+        }
+    }
+}
